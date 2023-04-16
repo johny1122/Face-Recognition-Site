8fa285f66a9116f858e707ccff65e17c26626ed1
Jonathan Birnbaum <58553700+johny1122@users.noreply.github.com>
2023-04-16 23:22:25 +0300

    update server url

--- src/components/Register/Register.js
+++ src/components/Register/Register.js
@@ -24,7 +24,7 @@ class Register extends React.Component{
     }
 
     onRegister = () => {
-        fetch('http://localhost:3000/register', {
+        fetch('https://face-recognition-server-2na4.onrender.com/register', {
             method: 'post',
             headers: {'Content-Type': 'application/json'},
             body: JSON.stringify({
--- src/components/Signin/Signin.js
+++ src/components/Signin/Signin.js
@@ -20,7 +20,7 @@ class Signin extends React.Component {
     }
 
     onSubmitSignIn = () => {
-        fetch('http://localhost:3000/signin', {
+        fetch('https://face-recognition-server-2na4.onrender.com/signin', {
             method: 'post',
             headers: {'Content-Type': 'application/json'},
             body: JSON.stringify({
